7b44cdba59521f623f6390468e3bcbf126c75ea3
Siddhartha Sahai <siddharthasahai99@gmail.com>
2018-04-30 22:48:35 +0530

    lab 1 part 1

--- mapreduce/common_reduce.go
+++ mapreduce/common_reduce.go
@@ -1,5 +1,12 @@
 package mapreduce
 
+import (
+	"os"
+	"io"
+	"log"
+	"encoding/json"
+)
+
 func doReduce(
 	jobName string, // the name of the whole MapReduce job
 	reduceTask int, // which reduce task this is
@@ -7,41 +14,50 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	//
-	// doReduce manages one reduce task: it should read the intermediate
-	// files for the task, sort the intermediate key/value pairs by key,
-	// call the user-defined reduce function (reduceF) for each key, and
-	// write reduceF's output to disk.
-	//
-	// You'll need to read one intermediate file from each map task;
-	// reduceName(jobName, m, reduceTask) yields the file
-	// name from map task m.
-	//
-	// Your doMap() encoded the key/value pairs in the intermediate
-	// files, so you will need to decode them. If you used JSON, you can
-	// read and decode by creating a decoder and repeatedly calling
-	// .Decode(&kv) on it until it returns an error.
-	//
-	// You may find the first example in the golang sort package
-	// documentation useful.
-	//
-	// reduceF() is the application's reduce function. You should
-	// call it once per distinct key, with a slice of all the values
-	// for that key. reduceF() returns the reduced value for that key.
-	//
-	// You should write the reduce output as JSON encoded KeyValue
-	// objects to the file named outFile. We require you to use JSON
-	// because that is what the merger than combines the output
-	// from all the reduce tasks expects. There is nothing special about
-	// JSON -- it is just the marshalling format we chose to use. Your
-	// output code will look something like this:
-	//
-	// enc := json.NewEncoder(file)
-	// for key := ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
-	//
-	// Your code here (Part I).
-	//
+	// empty array for collecting values
+	valuesList := make(map[string][]string)
+
+	for m := 0 ; m < nMap ; m++ {
+		// get the intermediate filename
+		fileName := reduceName(jobName, m, reduceTask)
+
+		// decode the file
+		tempFile, err := os.Open(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		dec := json.NewDecoder(tempFile)
+
+		// read all the values and append to list
+		for {
+			var val KeyValue
+			err := dec.Decode(&val)
+
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				log.Fatal(err)
+				break
+			}
+
+			// append the value read to the corresponding key
+			valuesList[val.Key] = append(valuesList[val.Key], val.Value)
+		}
+	}
+
+	// create the final file required
+	finalFile, err := os.Create(outFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer finalFile.Close()
+
+	// connect the encoder with the final output file
+	enc := json.NewEncoder(finalFile)
+
+	// loop through each intermediate key and write the json stream to the output file
+	for k, v := range valuesList {
+		enc.Encode(KeyValue{k, reduceF(k, v)})
+	}
 }
